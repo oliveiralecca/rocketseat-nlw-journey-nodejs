455fec7e1cdbe033e9f2649c3f3d8d03f1e90993
Letícia Oliveira <oliveiraslc@yahoo.com.br>
2024-09-10 01:21:23 -0400

    feat: create participants and trips routes

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,13 @@ import { createActivity } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activities";
 import { createLink } from "./routes/create-link";
 import { getLinks } from "./routes/get-links";
+import { getParticipants } from "./routes/get-participants";
+import { createInvite } from "./routes/create-invite";
+import { updateTrip } from "./routes/update-trip";
+import { getTripDetails } from "./routes/get-trip-details";
+import { getParticipant } from "./routes/get-participant";
+
+// COMMENT: minuto 15 da aula 3
 
 const app = fastify()
 
@@ -25,6 +32,11 @@ app.register(createActivity)
 app.register(getActivities)
 app.register(createLink)
 app.register(getLinks)
+app.register(getParticipants)
+app.register(createInvite)
+app.register(updateTrip)
+app.register(getTripDetails)
+app.register(getParticipant)
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running!")
