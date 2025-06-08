a6cb9f5032c30dde28409f682d46d6da6b5faa0d
Letícia Oliveira <oliveiraslc@yahoo.com.br>
2025-06-07 22:07:57 -0300

    refactor: tailor to the mobile project

--- src/routes/confirm-participant.ts
+++ src/routes/confirm-participant.ts
@@ -2,42 +2,57 @@ import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
-import { ClientError } from "../errors/client-error";
 import { env } from "../env";
 
-export async function confirmParticipant(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().get(
-    '/participants/:participantId/confirm',
+export const confirmParticipant = async (app: FastifyInstance) => {
+  app.withTypeProvider<ZodTypeProvider>().patch(
+    "/participants/:participantId/confirm",
     {
       schema: {
+        tags: ["participants"],
+        summary: "Confirms a participant on a trip.",
         params: z.object({
           participantId: z.string().uuid(),
-        })
-      }
-    }, 
+        }),
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+        }),
+        response: {
+          204: z.null(),
+          400: z.object({ message: z.string() }).describe("Bad request"),
+        },
+      },
+    },
     async (request, reply) => {
-      const { participantId } = request.params
+      const { participantId } = request.params;
+      const { name, email } = request.body;
 
       const participant = await prisma.participant.findUnique({
         where: {
-          id: participantId
-        }
-      })
+          id: participantId,
+        },
+      });
 
       if (!participant) {
-        throw new ClientError('Participant not found.')
+        throw new Error("Participant not found.");
       }
 
-      if (participant.is_confirmed) {
-        return reply.redirect(`${env.WEB_BASE_URL}/trips/${participant.trip_id}`)
-      }
+      const tripDetailsURL = new URL(
+        `/trips/${participant.trip_id}`,
+        env.WEB_BASE_URL
+      );
 
       await prisma.participant.update({
         where: { id: participantId },
-        data: { is_confirmed: true }
-      })
+        data: {
+          is_confirmed: true,
+          name,
+        },
+      });
 
-      return reply.redirect(`${env.WEB_BASE_URL}/trips/${participant.trip_id}`)
+      //return reply.redirect(tripDetailsURL.toString())
+      return reply.status(204).send();
     }
-  )
-}
+  );
+};
--- src/routes/create-invite.ts
+++ src/routes/create-invite.ts
@@ -10,7 +10,7 @@ import { env } from "../env";
 
 export async function createInvite(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
-    '/trips/:tripId/invites', 
+    "/trips/:tripId/invites",
     {
       schema: {
         params: z.object({
@@ -18,61 +18,65 @@ export async function createInvite(app: FastifyInstance) {
         }),
         body: z.object({
           email: z.string().email(),
-        })
-      }
-    }, 
+        }),
+      },
+    },
     async (request) => {
-      const { tripId } = request.params
-      const { email } = request.body
+      const { tripId } = request.params;
+      const { email } = request.body;
 
       const trip = await prisma.trip.findUnique({
-        where: { id: tripId }
-      })
+        where: { id: tripId },
+      });
 
       if (!trip) {
-        throw new ClientError('Trip not found.')
+        throw new ClientError("Trip not found.");
       }
 
       const participant = await prisma.participant.create({
         data: {
           email,
-          trip_id: tripId
-        }
-      })
+          trip_id: tripId,
+        },
+      });
 
-      const formattedStartDate = dayjs(trip.starts_at).format('LL')
-      const formattedEndDate = dayjs(trip.ends_at).format('LL')
+      const formattedStartDate = dayjs(trip.starts_at).format("LL");
+      const formattedEndDate = dayjs(trip.ends_at).format("LL");
 
-      const mail = await getMailClient()
-     
-      const confirmationLink = `${env.API_BASE_URL}/participants/${participant.id}/confirm`
+      const mail = await getMailClient();
+
+      const confirmationLink = new URL(
+        `planner://trip/${trip.id}?participant=${participant.id}`,
+        env.WEB_BASE_URL
+      );
 
       const message = await mail.sendMail({
         from: {
-          name: 'Equipe plann.er',
-          address: 'oi@plann.er',
+          name: "Equipe plann.er",
+          address: "oi@plann.er",
         },
         to: participant.email,
         subject: `Confirme sua presença na viagem para ${trip.destination} em ${formattedStartDate}`,
         html: `
           <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
-            <p>Você foi convidado(a) para participar de uma viagem para <strong>${trip.destination}</strong> nas datas <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong>.</p>
+            <p>Você foi convidado(a) para participar de uma viagem para <strong>${
+              trip.destination
+            }</strong> nas datas <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong>.</p>
             <p></p>
             <p>Para confirmar sua presença na viagem, clique no link abaixo:</p>
             <p></p>
             <p>
-              <a href="${confirmationLink}">Confirmar viagem</a>
+              <a href="${confirmationLink.toString()}">Confirmar viagem</a>
             </p>
             <p></p>
             <p>Caso você não saiba do que se trata esse e-mail, apenas ignore-o.</p>
           </div>
         `.trim(),
-      })
-  
-      console.log(nodemailer.getTestMessageUrl(message))
-   
+      });
 
-      return { participantId: participant.id }
+      console.log(nodemailer.getTestMessageUrl(message));
+
+      return { participantId: participant.id };
     }
-  )
+  );
 }
